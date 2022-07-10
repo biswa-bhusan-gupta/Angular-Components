9ba3df4001b70624257abdc2db09bc6e8ff10a05
Biswa Bhusan Gupta <66106600+biswa-bhusan-gupta@users.noreply.github.com>
2022-07-10 12:54:06 +0530

    First Commit

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,7 +3,14 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-card",
   // selector: ".app-card", // APP CARD AS CLASS
-  templateUrl: "./card.component.html"
+  templateUrl: "./card.component.html",
+  styles: [
+    `
+      h2 {
+        color: green;
+      }
+    `
+  ]
 })
 export class CardComponent {
   component = "Card Component";
